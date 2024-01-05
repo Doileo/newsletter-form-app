1266dd9b4095b7e714fc51afe87cebc4729ab824
doileo <leovchind@gmail.com>
2024-01-05 23:24:37 +0300

    Add FeaturesList component structure(image display issue)

--- src/components/FeaturesList.js
+++ src/components/FeaturesList.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+// Sample data for features
+const featuresData = [
+  {
+    description: "Product discovery and building what matters",
+  },
+  {
+    description: "Measuring to ensure updates are a success",
+  },
+  {
+    description: "And much more!"
+  },
+];
+
 export default function FeaturesList() {
-  return <div>FeaturesList component content</div>;
+  return (
+    <div>
+      <ul>
+        {featuresData.map((feature, index) => (
+          <li key={index}>
+            <img src="../images/icon-list.png" alt="Icon" />
+            <p>{feature.description}</p>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
