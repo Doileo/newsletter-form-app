0f5f4b31511592f77919a1cd380494901f82271b
doileo <leovchind@gmail.com>
2024-01-08 22:13:48 +0300

    Added SuccessMessage component with subscription confirmation and dismissal button

--- src/components/SuccessMessage.js
+++ src/components/SuccessMessage.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import successIcon from "../images/icon-success.png"
 
 export default function SuccessMessage() {
-  return <div>SuccessMessage component content</div>;
+  return (
+    <div>
+      <img src={successIcon} alt="A success icon." className="success--icon" />
+      <h2 className="success--header">Thanks for subscribing!</h2>
+      <p className="success--message">A confirmation email has been sent to <span className="strong">ash@loremcompany.com</span>. Please open it and click the button inside to confirm your subscription.</p>
+      <button>Dismiss message</button>
+    </div>
+  )
 }
