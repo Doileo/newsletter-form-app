4ee121a1db523c55d916bb6025c7591d68426f34
doileo <leovchind@gmail.com>
2024-01-09 18:08:19 +0300

    Added classNames for styling in the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import mobileIllustration from "../images/illustration-sign-up-mobile.png";
 
 export default function Header() {
   return (
-    <div>
-        <img src={mobileIllustration} alt="A graphic." />
-        <h1>Stay updated!</h1>
-        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+    <div className="hero">
+        <img src={mobileIllustration} alt="A graphic." className="hero--image" />
+        <h1 className="hero--header">Stay updated!</h1>
+        <p className="hero--text">Join 60,000+ product managers receiving monthly updates on:</p>
     </div>
   );
 }
