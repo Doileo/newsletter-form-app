6728c53aa4ff946138fdb2d0dc68b7bf3315dcdc
Doina <leovchind@gmail.com>
2025-03-17 14:52:05 +0200

    fix(accessibility): improved accessibility for icons and form inputs

--- src/components/SuccessMessage.js
+++ src/components/SuccessMessage.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import successIcon from '../images/icon-success.png';
+import React from "react";
+import successIcon from "../images/icon-success.png";
 
 export default function SuccessMessage({ onDismiss }) {
-  const handleDismiss = () => {
-    // Call the onDismiss function passed from the parent component (App.js)
-    onDismiss()
-  }
   return (
-    <div className="success--container">
-      <img src={successIcon} alt="A success icon." className="success--icon" />
-      <h2 className="success--header">Thanks for subscribing!</h2>
-      <p className="success--message">
-        A confirmation email has been sent to{' '}
-        <span className="strong">ash@loremcompany.com</span>. Please open it and click the button
-        inside to confirm your subscription.
+    <section className="success-container">
+      <img src={successIcon} alt="" className="success-icon" aria-hidden true />
+      <h2 className="success-header">Thanks for subscribing!</h2>
+      <p className="success-message">
+        A confirmation email has been sent to{" "}
+        <span className="strong">your email</span>. Please open it and click the
+        button inside to confirm your subscription.
       </p>
-      <button onClick={handleDismiss}>Dismiss message</button>
-    </div>
+      <button onClick={onDismiss} aria-label="Dismiss message">
+        Dismiss
+      </button>
+    </section>
   );
 }
-
